c6fc900fc94ffed74fe8ee6080d071a576003734
Zongze Wu <astrowu61@gmail.com>
2025-10-17 21:00:54 -0700

    Initialize the project file structure

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>SpeakTeX</h1>
+      <p>Convert spoken math to LaTeX</p>
+    </div>
+  );
+}
+
+export default App;
